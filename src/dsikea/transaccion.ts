03fbd9b89d3bbc3e30249fd412dc48cc67c364c2
José Ángel Marrero Domínguez <alu0101494500@ull.edu.es>
2024-03-12 23:51:35 +0000

    Preparación repo

--- src/dsikea/transaccion.ts
+++
@@ -1,7 +0,0 @@
-export interface Transaccion {
-	date: Date;
-	id_mueble: number;
-	id_implicado : number;
-	type: 'Compra' | 'Venta' | 'Devolución de Cliente' | 'Devolución a Proveedor';
-	amount: number;
-}
